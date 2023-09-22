1b8939133c3a471c22acc372189c229264cd6269
Eldrich Jyuri Sumabat <eldrichjyuris@gmail.com>
2023-09-22 19:16:58 +0800

    edit navbar component

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
-import './Search.css';
 
 
 function Search() {
   return (
-    <div className='absolute inset-x-0 top-80'>
-          <h2 className="text-xl font-semibold leading-7 text-gray-900 title">Find Home That Fits To You.</h2>
-          <form className='form'>
-            <input type="text" placeholder='search' className='border-2  border-blue-300 p-3 w-1/2 search_bar'>
+    <div className='absolute inset-x-0 top-72'>
+          <h2 className="text-xl text-white py-2 font-semibold leading-7 text-gray-900 flex justify-center">Find Home That Fits To You.</h2>
+          <form className='flex justify-center'>
+            <input type="text" placeholder='search' className='border-2  border-blue-300 p-3 w-1/2 '>
                 
             </input>
             <button className="btn px-2 bg-blue-300">Submit</button>
