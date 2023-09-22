ffe65215e0f4b3bb73c8413a183aece250c303c5
Eldrich Jyuri Sumabat <eldrichjyuris@gmail.com>
2023-09-22 18:10:17 +0800

    edit components

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,16 +1,21 @@
 import React from 'react'
 import './Search.css';
+import Image1 from './img/bg1.jpg'
+
 
 function Search() {
   return (
-    <div className='mt-6'>
-      <h2 className="text-xl font-semibold leading-7 text-gray-900 title">Find Home that fits to you.</h2>
-      <form className='form'>
-        <input type="text" placeholder='search' className='border-2 p-3 w-1/2 search_bar'>
-            
-        </input>
-        <button className="btn px-2 bg-blue-300">Submit</button>
-      </form>
+    <div>
+      <img src={Image1} alt="" className='image'/>
+        <div className='mt-6 search_bar'>
+          <h2 className="text-xl font-semibold leading-7 text-gray-900 title">Find Home That Fits To You.</h2>
+          <form className='form'>
+            <input type="text" placeholder='search' className='border-2  border-blue-300 p-3 w-1/2 search_bar'>
+                
+            </input>
+            <button className="btn px-2 bg-blue-300">Submit</button>
+          </form>
+        </div>
     </div>
   )
 }
