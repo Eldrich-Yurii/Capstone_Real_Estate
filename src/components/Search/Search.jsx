5f1e59b4e07272886eb5e70252f8baa301cb6463
Eldrich Jyuri Sumabat <eldrichjyuris@gmail.com>
2023-09-22 23:43:44 +0800

    edit respnsiveness

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -3,10 +3,10 @@ import Icon from './Icon';
 
 function Search() {
   return (
-    <div className='absolute inset-x-0 top-72'>
-          <h2 className="text-xl text-white py-2 px-2 font-semibold flex justify-center drop-shadow-md">Find A Home That Fits To You.</h2>
+    <div className='absolute inset-x-0 sm:top-36 md:50 lg:top-72'>
+          <h2 className="text-xl text-white py-2 px-2 font-semibold flex justify-center drop-shadow-md sm:text-sm">Find A Home That Fits To You.</h2>
           <form className='flex justify-center drop-shadow-lg'>
-            <input type="text" placeholder='You Can Search Properties By: Location, Property Type, or Income... ' className='border-2  border-purple-900 p-3 w-1/2 rounded-bl-3xl'/>
+            <input type="text" placeholder='You Can Search Properties By: Location, Property Type, or Income... ' className='border-2  border-purple-900 p-3 w-1/2 rounded-bl-3xl sm:w-50'/>
             <button className="btn font-semibold px-4 bg-purple-900 text-white rounded-tr-3xl hover:bg-purple-600 hover:drop-shadow-xl hover:text-black"><Icon /></button>
           </form>
     </div>
