af9fecc5a1d755b452fafef43641de27afd2dd0c
Eldrich Jyuri Sumabat <eldrichjyuris@gmail.com>
2023-09-24 17:36:50 +0800

    edit quote component and added plug ins

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -4,15 +4,21 @@ function Cards() {
   return (
     <>
       <div className="m-10 drop-shadow-2xl relative group duration-500 cursor-pointer group overflow-hidden relative text-gray-50 h-72 w-56  rounded-2xl hover:duration-700 hover:shadow-2xl">
-        <div className="w-56 h-72 bg-gray-50 text-gray-">
+        <div className="w-56 h-72 bg-purple-200 text-gray-">
           <div className="flex flex-row justify-between">
-          
+              
           </div>
       </div>
-        <div className="absolute bg-purple-300 -bottom-24 w-56 p-3 flex flex-col gap-1 group-hover:-bottom-0 group-hover:duration-600 duration-500 hover:bg-purple-400">
-          <span className="text-whote font-bold text-sm">Specific Locations...</span>
-          <span className="text-gray-800 font-bold text-3xl">Cheat Sheet</span>
-          <p className="text-neutral-800">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        <div className="absolute bg-purple-400 -bottom-24 w-56 p-3 flex flex-col gap-1 group-hover:-bottom-0 group-hover:duration-600 duration-500 group-hover:bg-purple-700">
+          <span className="text-white font-bold text-sm pb-3">Locations areas...</span>
+          <hr />
+          <p className="text-neutral-800">
+            <ul className='text-white'>
+              <li>Sta. Maria</li>
+              <li> Marilao</li>
+              <li>Meycauayan and more...</li>
+            </ul>
+            </p>
         </div>  
       </div>
     </>
