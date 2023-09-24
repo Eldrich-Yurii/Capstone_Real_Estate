8c360f502dd99209859eb3f48fc5dd13d68325ef
Eldrich Jyuri Sumabat <eldrichjyuris@gmail.com>
2023-09-24 11:02:47 +0800

    experiment search bar

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -6,7 +6,7 @@ function Search() {
     <div className='absolute inset-x-0 sm:top-16 md:50 lg:top-72'>
           <h2 className="text-xl text-white py-2 px-2 font-semibold flex justify-center drop-shadow-md sm:text-sm">Find A Home That Fits To You.</h2>
           <form className='flex justify-center drop-shadow-lg'>
-            <input type="text" placeholder='You Can Search Properties By: Location, Property Type, or Income... ' className='border-2 border-purple-900 p-3 rounded-bl-3xl w-1/2 sm:w-20 sm:h-10'/>
+            <input type="text" placeholder='You Can Search Properties By: Location, Property Type, or Income... ' className='border-2 border-purple-900 p-3 rounded-bl-3xl sm:w-50 sm:h-10 md:w-1/2 lg:w-1/2 lg:h-12'/>
             <button className="btn font-semibold px-4 bg-purple-900 text-white rounded-tr-3xl hover:bg-purple-600 hover:drop-shadow-xl hover:text-black"><Icon /></button>
           </form>
     </div>
