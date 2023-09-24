288665b6de1ca7f56a95a100aec96674551c4858
Eldrich Jyuri Sumabat <eldrichjyuris@gmail.com>
2023-09-24 17:44:07 +0800

    remove package

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -6,7 +6,6 @@ function Cards() {
       <div className="m-10 drop-shadow-2xl relative group duration-500 cursor-pointer group overflow-hidden relative text-gray-50 h-72 w-56  rounded-2xl hover:duration-700 hover:shadow-2xl">
         <div className="w-56 h-72 bg-purple-200 text-gray-">
           <div className="flex flex-row justify-between">
-              
           </div>
       </div>
         <div className="absolute bg-purple-400 -bottom-24 w-56 p-3 flex flex-col gap-1 group-hover:-bottom-0 group-hover:duration-600 duration-500 group-hover:bg-purple-700">
