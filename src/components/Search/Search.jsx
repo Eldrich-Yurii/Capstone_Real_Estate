219345ef6442da8c47721cf6af3135101b1d99a5
Eldrich Jyuri Sumabat <eldrichjyuris@gmail.com>
2023-09-25 15:42:58 +0800

    edit responsiveness

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -3,11 +3,11 @@ import Icon from './Icon';
 
 function Search() {
   return (
-    <div className='absolute inset-x-0 sm:top-16 md:50 lg:top-72'>
-          <h2 className="text-xl text-white py-2 px-2 font-semibold flex justify-center drop-shadow-md sm:text-sm">Find A Home That Fits To You.</h2>
+    <div className='absolute inset-x-0 top-16'>
+          <h2 className="text-sm text-white py-2 px-2 font-semibold flex justify-center drop-shadow-md">Find A Home That Fits To You.</h2>
           <form className='flex justify-center drop-shadow-lg'>
-            <input type="text" placeholder='You Can Search Properties By: Location, Property Type, or Income... ' className='border-2 border-purple-900 p-3 rounded-bl-3xl sm:w-50 sm:h-10 md:w-1/2 lg:w-1/2 lg:h-12'/>
-            <button className="btn font-semibold px-4 bg-purple-900 text-white rounded-tr-3xl hover:bg-purple-600 hover:drop-shadow-xl hover:text-black"><Icon /></button>
+            <input type="text" placeholder='You Can Search Properties By: Location, Property Type, or Income... ' className='border-2 border-purple-900 p-3 rounded-bl-3xl h-10'/>
+            <button className="btn font-semibold px-4 bg-purple-900 text-white rounded-tr-3xl hover:bg-gradient-to-r from-purple-600 to-cyan-500 hover:shadow-2xl hover:text-black duration-500"><Icon /></button>
           </form>
     </div>
   )
