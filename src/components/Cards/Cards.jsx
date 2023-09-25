5b289677110be1689d9404a0cbdfadfc65cc3c79
Eldrich Jyuri Sumabat <eldrichjyuris@gmail.com>
2023-09-25 22:26:29 +0800

    change cyan colors to purple

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -16,16 +16,14 @@ function Cards() {
               </div>
               <span className='text-black text-xl font-bold flex justify-center pt-4'>BULACAN</span>
             </div>
-            <div className="absolute bg-cyan-600 -bottom-24 w-80 p-3 flex flex-col gap-1 group-hover:-bottom-0 group-hover:duration-600 duration-500 group-hover:bg-purple-700">
+            <div className="absolute bg-purple-600 -bottom-24 w-80 p-3 flex flex-col gap-1 group-hover:-bottom-0 group-hover:duration-600 duration-500 group-hover:bg-purple-900">
               <span className="text-white font-bold text-sm pb-3">Locations areas...</span>
                     <hr />
-                  <p className="text-neutral-800">
-                    <ul className='text-white'>
-                      <li>Sta. Maria</li>
-                      <li> Marilao</li>
-                      <li>Meycauayan and more...</li>
-                    </ul>
-                  </p>
+                <ul className='text-white'>
+                  <li>Sta. Maria</li>
+                  <li> Marilao</li>
+                  <li>Meycauayan and more...</li>
+                </ul> 
             </div>  
           </div>
           <div className="m-10 drop-shadow-2xl group duration-500 cursor-pointer group overflow-hidden relative text-gray-50 h-96 w-80  rounded-2xl hover:duration-700 hover:shadow-2xl">
@@ -35,7 +33,7 @@ function Cards() {
               </div>
               <span className='text-black text-xl font-bold flex justify-center pt-4'>METRO MANILA</span>
             </div>
-            <div className="absolute bg-cyan-600 -bottom-24 w-80 p-3 flex flex-col gap-1 group-hover:-bottom-0 group-hover:duration-600 duration-500 group-hover:bg-purple-700">
+            <div className="absolute bg-purple-600 -bottom-24 w-80 p-3 flex flex-col gap-1 group-hover:-bottom-0 group-hover:duration-600 duration-500 group-hover:bg-purple-900">
               <span className="text-white font-semibold text-sm pb-3">Locations areas...</span>
                       <hr />
                   <p className="text-neutral-800">
@@ -54,7 +52,7 @@ function Cards() {
               </div>
               <span className='text-black text-xl font-bold flex justify-center pt-4'>CAVITE</span>
             </div>
-            <div className="absolute bg-cyan-600 -bottom-24 w-80 p-3 flex flex-col gap-1 group-hover:-bottom-0 group-hover:duration-600 duration-500 group-hover:bg-purple-700">
+            <div className="absolute bg-purple-600 -bottom-24 w-80 p-3 flex flex-col gap-1 group-hover:-bottom-0 group-hover:duration-600 duration-500 group-hover:bg-purple-900">
               <span className="text-white font-bold text-sm pb-3">Locations areas...</span>
                         <hr />
                   <p className="text-neutral-800">
--- src/components/Quote/Quote.jsx
+++ src/components/Quote/Quote.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Quote() {
   return (
     <div>
-      <span className='text-2xl text-white font-bold h-24 md:lg:text-4xl md:lg:h-48 bg-gradient-to-b from-purple-900 to-violet-600 flex justify-center items-center'>
+      <span className='text-2xl text-white font-bold h-24 md:lg:text-4xl md:lg:h-48 bg-purple-900 flex justify-center items-center'>
         <del className='text-black font-extrabold'>
         STOP RENTING.</del> &#160;&#160;&#160;&#160;&#160;&#160;<u className='font-extrabold'>START OWNING.</u> 
       </span>
