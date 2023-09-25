020182527130f605462a79d5dba5ba09899610f9
Eldrich Jyuri Sumabat <eldrichjyuris@gmail.com>
2023-09-25 21:05:09 +0800

    change gradient to monotone

--- src/components/Quote/Quote.jsx
+++ src/components/Quote/Quote.jsx
@@ -3,8 +3,9 @@ import React from 'react'
 function Quote() {
   return (
     <div>
-      <span className='text-2xl text-white font-bold h-24 md:lg:text-4xl md:lg:h-48 bg-gradient-to-b from-purple-900 to-cyan-700 flex justify-center items-center'><del className='text-black font-extrabold'>
-        STOP RENTING.</del> &#160;<u className='font-extrabold'>START OWNING.</u> 
+      <span className='text-2xl text-white font-bold h-24 md:lg:text-4xl md:lg:h-48 bg-gradient-to-b from-purple-900 to-violet-600 flex justify-center items-center'>
+        <del className='text-black font-extrabold'>
+        STOP RENTING.</del> &#160;&#160;&#160;&#160;&#160;&#160;<u className='font-extrabold'>START OWNING.</u> 
       </span>
     </div>
   )
