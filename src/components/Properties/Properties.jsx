fd28ba9a175d3333b7d7ef974222a4ff956e4aec
Eldrich Jyuri Sumabat <eldrichjyuris@gmail.com>
2023-09-27 19:24:49 +0800

    edit margin

--- src/components/Properties/Properties.jsx
+++ src/components/Properties/Properties.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Properties() {
   return (
-    <div className=''>
+    <div className='mt-24 mb-36'>
       <h1 className='pt-7 pl-5 text-2xl md:lg:text-3xl font-bold'>Properties</h1>  
     <div className='relative flex justify-center pt-10 gap-10 overflow-x-scroll'>
       <div className='w-56 h-56 bg-purple-500'>
